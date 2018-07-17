a6aee263727813a3fd3354cc2751c3871ea00701
anvesh <anveshkumar.perugu@gmail.com>
2018-07-16 23:07:55 -0400

    added configuration file

--- src/config.py
+++ src/config.py
@@ -6,5 +6,5 @@ import os
 ########################
 
 PATH_MAIN = os.path.dirname(os.path.realpath(__file__))
-PATH_INPUT_FILE = os.path.join(PATH_MAIN, 'input/itcont.txt')
-PATH_OUTPUT_FILE = os.path.join(PATH_MAIN, 'output/ph_out.txt')
+PATH_INPUT_FILE = os.path.join(PATH_MAIN, '../input/itcont.txt')
+PATH_OUTPUT_FILE = os.path.join(PATH_MAIN, '../output/ph_out.txt')
